9562c17c84629002cfe4542fa6182767a8adb32f
benyy0101 <benyy0101@gmail.com>
2023-02-07 19:53:35 +0900

    suspense& lazy added

--- 21.authentication/src/App.js
+++ 21.authentication/src/App.js
@@ -4,34 +4,41 @@ import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
-import {useContext} from "react";
+import React, {useContext, Suspense} from "react";
 import AuthContext from "./store/auth-context";
 
+const HomePage = React.lazy(()=>{
+    return import('./pages/HomePage');
+})
 
 function App() {
 
     const authCtx = useContext(AuthContext);
 
     return (
-        <Layout>
-            <Switch>
-                <Route path='/' exact>
-                    <HomePage/>
-                </Route>
-                {/* 316. Protecting auth page*/}
-                {!authCtx.isLoggedIn && <Route path='/auth'>
-                    <AuthPage/>
-                </Route>}
-                {authCtx.isLoggedIn && <Route path='/profile'>
-                    <UserProfile/>
-                </Route>}
+        <Suspense fallback={
+            <div className='centered'>Loading...</div>
+        }>
+            <Layout>
+                <Switch>
+                    <Route path='/' exact>
+                        <HomePage/>
+                    </Route>
+                    {/* 316. Protecting auth page*/}
+                    {!authCtx.isLoggedIn && <Route path='/auth'>
+                        <AuthPage/>
+                    </Route>}
+                    {authCtx.isLoggedIn && <Route path='/profile'>
+                        <UserProfile/>
+                    </Route>}
 
-              <Route path='*'>
-                <Redirect to='/'/>
-              </Route>
+                    <Route path='*'>
+                        <Redirect to='/'/>
+                    </Route>
 
-            </Switch>
-        </Layout>
+                </Switch>
+            </Layout>
+        </Suspense>
     );
 }
 
